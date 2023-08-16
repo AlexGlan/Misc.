2b8c02c6eb34a46c72a8173af7ce69cfe2eff133
Alex <alex2531458@gmail.com>
2023-08-16 15:51:55 +0300

    Add BuildArrayFromPermutation.java

---
+++ Leetcode/Java/BuildArrayFromPermutation.java
@@ -0,0 +1,12 @@
+class BuildArrayFromPermutation {
+    public int[] buildArray(int[] nums) {
+        int[] ans;
+        ans = new int[nums.length];
+
+        for(int i = 0; i < ans.length; i++) {
+            ans[i] = nums[nums[i]];
+        }
+
+        return ans;            
+    }
+}
