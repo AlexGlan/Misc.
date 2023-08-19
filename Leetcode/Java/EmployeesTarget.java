c2da4f8b04c74d168a658a42618f5315ecd7734b
Alex <alex2531458@gmail.com>
2023-08-19 09:55:50 +0300

    Add EmployeesTarget.java

---
+++ Leetcode/Java/EmployeesTarget.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int num = 0;
+
+        for (int i : hours) {
+            if (i >= target) {
+                num += 1;
+            }
+        }
+        
+        return num;        
+    }
+}
