ad3345c1e260885f76fd2b347a3c23eefd06cfe7
Alex Glancevs <alex2531458@gmail.com>
2024-07-16 12:53:00 +0300

    feat: add solution for 2627

---
+++ Leetcode/Typescript/debounce.ts
@@ -0,0 +1,10 @@
+type F = (...args: number[]) => void
+
+function debounce(fn: F, t: number): F {
+    let timer: ReturnType<typeof setTimeout>;
+
+    return function(...args) {
+        clearTimeout(timer);
+        timer = setTimeout(fn, t, ...args);
+    }
+};
